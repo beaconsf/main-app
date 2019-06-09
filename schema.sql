1b1151ad978a5e966f3e9f14e71a5475cd4589c4
Greg Kittleson <greg.kittleson@gmail.com>
2019-06-09 16:11:38 -0700

    Seed database with categories and sample event

--- schema.sql
+++ schema.sql
@@ -15,8 +15,8 @@ CREATE TABLE events (
   title varchar(100) NOT NULL,
   start_time DATETIME NOT NULL,
   end_time DATETIME NOT NULL,
-  lng DECIMAL(9,6) NOT NULL,
   lat DECIMAL(9,6) NOT NULL,
+  lng DECIMAL(9,6) NOT NULL,
   owner int,
   FOREIGN KEY (owner)
   REFERENCES users(id)
@@ -36,3 +36,17 @@ CREATE TABLE event_type (
   FOREIGN KEY (event_kind)
   REFERENCES categories(id)
 );
+
+
+
+INSERT INTO categories(kind) VALUES('shelter');
+INSERT INTO categories(kind) VALUES('food');
+INSERT INTO categories(kind) VALUES('medical');
+INSERT INTO categories(kind) VALUES('dental');
+INSERT INTO categories(kind) VALUES('selfCare');
+INSERT INTO categories(kind) VALUES('community');
+
+INSERT INTO users(username, org) VALUES('rubberducky', 'lava mae');
+INSERT INTO events(title, start_time, end_time, lat, lng, owner) VALUES('Get a shower and some free stuff!', '2019-06-09 08:00:00', '2019-06-09 13:30:00', 37.779200, -122.419000, 1);
+INSERT INTO event_type(event_id, event_kind) VALUES(1, 5);
+INSERT INTO event_type(event_id, event_kind) VALUES(1, 6);
