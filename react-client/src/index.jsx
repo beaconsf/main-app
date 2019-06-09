58185e5ebd0b853e8a6b51a904b8424a44ab9dbd
Angela Lee <rubii.kun@gmail.com>
2019-06-09 15:04:24 -0700

    Implement Sidebar framework
    Displays menu for categories, city location, and date.

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import Sidebar from './components/Sidebar.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       services: {
         shelter: [],
         food: [],
@@ -17,28 +17,30 @@ class App extends React.Component {
       },
       selectedCat: 'shelter',
       selectedEvent: {}
-    }
+    };
   }
 
   componentDidMount() {
     $.ajax({
-      url: '/date', 
-      success: (data) => {
+      url: '/date',
+      success: data => {
         this.setState({
           items: data
-        })
+        });
       },
-      error: (err) => {
+      error: err => {
         console.log('err', err);
       }
     });
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  render() {
+    return (
+      <div>
+        <h1>Beacon</h1>
+        <Sidebar />
+      </div>
+    );
   }
 }
 
